60a120182adbb02843992babadfe0930a47a8e9e
antonioenpidev <antonioenpidev@gmail.com>
2024-05-25 23:52:29 +0000

    JSX2-v8

--- segunda-app/src/App.js
+++ segunda-app/src/App.js
@@ -1,12 +1,23 @@
 import './App.css';
+import React, {useState} from 'react';
+
 
 function App() {
 
+  const [res, setRes] = useState(null)
+
   const elemento = <h1 className="centrar-titulo">Hola mundo</h1>
-  const elemento2 = suma(5, 5);
+  //const elemento2 = <div>{suma(5, 5)}</div>
+
+  const botonPulsado = () => {
+    const result = suma(7, 5);
+    setRes(result)
+  } 
+
   return <div>
+    <button onClick={botonPulsado} style={{marginTop:"10px", marginLeft: "10px"}}>Pulsame</button>
     <div>{elemento}</div>
-    <div>{elemento2}</div>
+    <div>{res != null && <h2>El resultado es: {res}</h2>}</div>
   </div>
 
 }
